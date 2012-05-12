311961fa3256d61f4cf26c45d1e1a49483f1f8f9
jcmellado@gmail.com <jcmellado@gmail.com@679f6105-17e0-df66-7101-ac66db937c53>
2012-05-12 17:38:30 +0000

    Initial version

---
+++ trunk/src/handtracking.js
@@ -0,0 +1,127 @@
+/*
+Copyright (c) 2012 Juan Mellado
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+var HT = HT || {};
+
+HT.Tracker = function(){
+  this.mask = new CV.Image();
+  this.eroded = new CV.Image();
+  this.contours = [];
+  
+  this.skinner = new HT.Skinner();
+};
+
+HT.Tracker.prototype.detect = function(image){
+  this.skinner.mask(image, this.mask);
+  
+  CV.erode(this.mask, this.eroded);
+  CV.dilate(this.eroded, this.mask);
+
+  this.contours = CV.findContours(this.mask);
+
+  return this.findCandidate(this.contours, image.width * image.height * 0.05, 0.005);
+};
+
+HT.Tracker.prototype.findCandidate = function(contours, minSize, epsilon){
+  var contour, candidate;
+  
+  contour = this.findMaxArea(contours, minSize);
+  if (contour){
+    contour = CV.approxPolyDP(contour, contour.length * epsilon);
+  
+    candidate = new HT.Candidate(contour);
+  }
+  
+  return candidate;
+};
+
+HT.Tracker.prototype.findMaxArea = function(contours, minSize){
+  var len = contours.length, i = 0,
+      minArea = -Infinity, area, contour;
+
+  for (; i < len; ++ i){
+    area = CV.area(contours[i]);
+    if (area >= minSize){
+    
+      if (area > minArea) {
+        minArea = area;
+      
+        contour = contours[i];
+      }
+    }
+  }
+  
+  return contour;
+};
+
+HT.Candidate = function(contour){
+  this.contour = contour;
+  this.hull = CV.convexHull(contour);
+  this.defects = CV.convexityDefects(contour, this.hull);
+};
+
+HT.Skinner = function(){
+};
+
+HT.Skinner.prototype.mask = function(imageSrc, imageDst){
+  var src = imageSrc.data, dst = imageDst.data, len = src.length,
+      i = 0, j = 0,
+      r, g, b, h, s, v, value;
+
+  for(; i < len; i += 4){
+    r = src[i];
+    g = src[i + 1];
+    b = src[i + 2];
+  
+    v = Math.max(r, g, b);
+    s = v === 0? 0: 255 * ( v - Math.min(r, g, b) ) / v;
+    h = 0;
+    
+    if (0 !== s){
+      if (v === r){
+        h = 30 * (g - b) / s;
+      }else if (v === g){
+        h = 60 + ( (b - r) / s);
+      }else{
+        h = 120 + ( (r - g) / s);
+      }
+      if (h < 0){
+        h += 360;
+      }
+    }
+    
+    value = 0;
+
+    if (v >= 15 && v <= 250){
+      if (h >= 3 && h <= 33){
+        value = 255;
+      }
+    }
+    
+    dst[j ++] = value;
+  }
+  
+  imageDst.width = imageSrc.width;
+  imageDst.height = imageSrc.height;
+  
+  return imageDst;
+};
